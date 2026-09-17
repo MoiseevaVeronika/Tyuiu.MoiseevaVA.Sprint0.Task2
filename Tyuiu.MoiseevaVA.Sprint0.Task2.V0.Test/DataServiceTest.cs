926203a77b2477c296707b019a72f11bcaf3d59e
Tyuiu.MoiseevaAV <vera.moiseeva.08@inbox.ru>
2026-09-17 08:29:42 +0500

    Task2

---
+++ Tyuiu.MoiseevaVA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.MoiseevaVA.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.MoiseevaVA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Вероника";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Вероника", res);
+
+        }
+    }
+}
---
+++ Tyuiu.MoiseevaVA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.MoiseevaVA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MoiseevaVA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Вероника"));
+            Console.ReadKey();
+        }
+    }
+}
